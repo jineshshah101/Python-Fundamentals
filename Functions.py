1b8b918285c9bb1262467ed48fc9edb9e939aae0
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-05 12:49:25 -0500

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,23 @@
+import turtle
+
+# made one turtle
+squirtle_turtle = turtle.Turtle()
+
+# creating a function to make a square
+def squirtle():
+    squirtle_turtle.forward(200)
+    squirtle_turtle.right(90)
+    squirtle_turtle.forward(200)
+    squirtle_turtle.right(90)
+    squirtle_turtle.forward(200)
+    squirtle_turtle.right(90)
+    squirtle_turtle.forward(200)
+    squirtle_turtle.showturtle()
+
+# call on the function to make the square
+squirtle()
+
+# making a pair of glasses
+squirtle_turtle.right(90)
+squirtle_turtle.forward(400)
+squirtle()
