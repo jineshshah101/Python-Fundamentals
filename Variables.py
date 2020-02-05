d40c988f3f974db457f2770e0eb853d4b5014c96
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-05 12:49:04 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,29 @@
+import turtle
+
+# basic ideas about variables
+
+a= 4
+print(a)
+
+b = "Yoo"
+print(b)
+
+check_result = 526489652
+print(check_result)
+
+# making a square using turtle
+
+# Note for the right distance of the turtle think of the turning as degrees. Make it turn 90 degrees for right angle
+# forward moves it forward
+# right means it turns
+# showturtle will show the turtle moving
+
+squirtle_turtle = turtle.Turtle()
+squirtle_turtle.forward(200)
+squirtle_turtle.right(90)
+squirtle_turtle.forward(200)
+squirtle_turtle.right(90)
+squirtle_turtle.forward(200)
+squirtle_turtle.right(90)
+squirtle_turtle.forward(200)
+squirtle_turtle.showturtle()
