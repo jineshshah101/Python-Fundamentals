642c6d2629f536eec3bf55ebcaf18eba3cc19e2b
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-06 16:11:13 -0500

    Add files via upload

---
+++ While Loop.py
@@ -0,0 +1,19 @@
+import turtle
+
+# made one turtle
+squirtle_turtle = turtle.Turtle()
+
+# creating a statement
+katakuri = "Best Character"
+
+# creating an infinite loop using while loop for the turtle to keep moving while katakuri is the best character
+while katakuri == "Best Character":
+    squirtle_turtle.forward(10)
+
+# creating a while loop for the turtle to keep moving while katakuri is the worst character
+# this is false without question
+
+while katakuri == "Worst Character":
+    squirtle_turtle.forward(10)
+
+
