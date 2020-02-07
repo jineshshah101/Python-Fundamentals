f953f20211ea123b7f696a5ce76132924cd5ef56
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-07 13:40:09 -0500

    Add files via upload

---
+++ String.py
@@ -0,0 +1,10 @@
+# Creating a string
+# A string is pretty much like a sentence, anything inside the quotations marks is a string, single quote also works for strings
+
+greed = "The Fox Sin of Greed, Sir Ban"
+print(greed)
+
+# if you want to print one chosen letter of the string you put it in brackets and say the value you want it to read
+# remember numbers start with 0 in programming
+
+print(greed[4])
