80cf314518829b652e018bf9d8afbbb27e085396
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-07 13:39:23 -0500

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,23 @@
+# Integer Data Type
+# numbers that aren't decimals
+
+four = 2 + 2
+print(four)
+
+# Float Data Type
+# Decimal numbers
+
+fivepointfive = 5.5
+print(fivepointfive)
+
+# String Data Type
+# Things in quotations are Strings
+
+Pride = "The Lion Sin of Pride, Sir Escanor"
+print(Pride)
+
+# Boolen Data Type
+# Either True or False ie Yes or No
+
+print(True)
+print(False)
