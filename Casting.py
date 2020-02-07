05610cbe2b8883aa3023ca3887c2c62e08e5e20c
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-02-07 13:39:51 -0500

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,32 @@
+# variable example
+
+x = 5
+print(x)
+
+# casting means changing one data type into another datatype
+# casting the integer x into a float
+
+y = float(x)
+print(y)
+
+# casting the integer x into a string
+
+z = str(x)
+print(z)
+
+# Note: Cannot cast a string that has letters in it to a integer. The string itself must be numbers
+# Casting string to an integer
+
+t = int("55")
+print(t)
+
+# for boolen if you have the value be 0 it will be false. Any other value except for 0 will come out as true
+
+h = bool(0)
+print(h)
+
+j = bool(1)
+print(j)
+
+k = bool(-5)
+print(k)
